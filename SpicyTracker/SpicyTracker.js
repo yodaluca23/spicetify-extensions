50a8040963a31e4b311c1b1fd506d130478f3da3
Luca LeBlanc <67206487+yodaluca23@users.noreply.github.com>
2025-01-22 14:05:06 -0600

    More Tracking parameter for SpicyTracker

--- SpicyTracker/SpicyTracker.js
+++ SpicyTracker/SpicyTracker.js
@@ -8,16 +8,19 @@
     const originalWriteText = navigator.clipboard.writeText;
     navigator.clipboard.writeText = function (text) {
         // Check if the text contains the '?si=' parameter
-        if (text.includes('?si=')) {
-            // Remove the '?si=' parameter and anything following it
-            const cleanedText = text.replace(/(\?si=[^&]*)/g, '');
-            return originalWriteText.call(navigator.clipboard, cleanedText);
+        if (text.includes('?si=') || text.includes('&si=') || 
+           text.includes('?pt=') || text.includes('&pt=') || 
+           text.includes('?context=') || text.includes('&context=')) {
+           // Remove the '?si=', '&si=', '?pt=', '&pt=', '?context=', '&context=' parameters and anything following them
+           const cleanedText = text.replace(/([?&](si|pt|context)=[^&]*)/g, '');
+           return originalWriteText.call(navigator.clipboard, cleanedText);
         }
+
 
         // If no modification needed, just call the original function
         return originalWriteText.call(navigator.clipboard, text);
     };
 
-    console.log("[SpicyTracker v1.0] Overwrote clipboard function.");
+    console.log("[SpicyTracker v1.1] Overwrote clipboard function.");
     
 })();
