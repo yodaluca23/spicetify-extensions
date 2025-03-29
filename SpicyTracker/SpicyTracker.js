007754f0bcb284d39aa03cf0348a9d22df697225
Luca LeBlanc <67206487+yodaluca23@users.noreply.github.com>
2025-03-28 21:55:33 -0500

    SpicyTracker 2.0 remove all params no regex
    Switched away from using regex, to instead have it try to turn it into a URL object. Then remove all the search parameters, I don't believe Spotify uses query parameters, except for tracking, on any of their copy actions, so this should work, please make an issue if it doesn't. It also catches and return the original text if it fails.

--- SpicyTracker/SpicyTracker.js
+++ SpicyTracker/SpicyTracker.js
@@ -1,26 +1,31 @@
 // NAME: SpicyTracker
 // AUTHOR: yodaluca23
-// DESCRIPTION: Remove tracking parameter "?si=" from Spotify share links.
+// DESCRIPTION: Remove tracking parameters such as "?si=" from Spotify share links.
 
 (function SpicyTracker() {
 
-    // Override clipboard write function
+    // Save the original writeText function
     const originalWriteText = navigator.clipboard.writeText;
+
+    // Overwrite the writeText function
     navigator.clipboard.writeText = function (text) {
-        // Check if the text contains the '?si=' parameter
-        if (text.includes('?si=') || text.includes('&si=') || 
-           text.includes('?pt=') || text.includes('&pt=') || 
-           text.includes('?context=') || text.includes('&context=')) {
-           // Remove the '?si=', '&si=', '?pt=', '&pt=', '?context=', '&context=' parameters and anything following them
-           const cleanedText = text.replace(/([?&](si|pt|context)=[^&]*)/g, '');
-           return originalWriteText.call(navigator.clipboard, cleanedText);
+        try {
+            // Try to parse the text as a URL
+            const url = new URL(text);
+
+            // Clear all parameters, Spotify never uses them other than for tracking I believe create an issue if this is not the case
+            url.search = '';
+
+            // Return the cleaned URL
+            return originalWriteText.call(navigator.clipboard, url.toString());
+            
+        } catch (e) {
+
+            // If it's not a valid URL, just call the original function
+            return originalWriteText.call(navigator.clipboard, text);
         }
-
-
-        // If no modification needed, just call the original function
-        return originalWriteText.call(navigator.clipboard, text);
     };
 
-    console.log("[SpicyTracker v1.1] Overwrote clipboard function.");
+    console.log("[SpicyTracker v2.0] Overwrote clipboard function.");
     
 })();
